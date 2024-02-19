785d923a58e9f6e1bd23a4a9dd4cbd4ceb8036e9
adilbek <231028@astanait.edu.kz>
2024-02-19 19:07:25 +0600

    first

---
+++ src/main/java/com/example/oopaer/models/Ticket.java
@@ -0,0 +1,35 @@
+public class Ticket {
+    private int id_gen;
+    private String name;
+    private int price;
+    private static int id = id_gen++;
+
+    public static int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public static void setId(int id) {
+        Ticket.id = id;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+@Override
+public String ToString {
+    return id + ". " + "Name of ticket: " + name + " price of ticket: " + price;
+}
