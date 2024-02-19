fc3b89714ad64ead0c30f0eed319ced9f771b656
rusyajkeee <230432@astanait.edu.kz>
2024-02-19 18:52:58 +0600

    new user class

--- src/main/java/com/example/oopaer/models/User.java
+++ src/main/java/com/example/oopaer/models/User.java
@@ -5,9 +5,35 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 @Setter
-@AllArgsConstructor
-@ToString
-@Getter
+@NoArgsConstructor
 public class User {
+    @Data
+    @Entity
+    @Table(name = "users")
+    public class User {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int id;
+        private String login;
+        private String password;
+        private boolean typeofaccount;
 
+        public int getId() {
+            return id;
+        }
+
+        public String getLogin() {
+            return login;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public boolean getTypeOfAccount() {
+            return typeofaccount;
+        }
+
+
+    }
 }
