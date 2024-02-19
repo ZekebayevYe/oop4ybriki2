5204c2005ac663516fddfc3db7eddc768bf9fbc8
Waitan <231427@astanait.edu.kz>
2024-02-19 16:35:14 +0600

    Imported lombok library

---
+++ src/main/java/com/example/oopaer/User.java
@@ -0,0 +1,13 @@
+package com.example.oopaer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+@Setter
+@AllArgsConstructor
+@ToString
+@Getter
+public class User {
+
+}
