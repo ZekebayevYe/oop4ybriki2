2f0dacd3a64da696057e45d814111a7140946503
Waitan <231427@astanait.edu.kz>
2024-02-19 16:35:14 +0600

    Начало положе... Но

---
+++ src/main/java/com/example/oopaer/OopAerApplication.java
@@ -0,0 +1,13 @@
+package com.example.oopaer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopAerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopAerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oopaer/OopAerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oopaer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopAerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
