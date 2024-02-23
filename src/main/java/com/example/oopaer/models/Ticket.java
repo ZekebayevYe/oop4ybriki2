cb7b8b87257d84f23ac88ce3619508d200d8f1b4
adilbek <231028@astanait.edu.kz>
2024-02-23 08:32:33 +0600

    ticket update

--- src/main/java/com/example/oopaer/models/Ticket.java
+++ src/main/java/com/example/oopaer/models/Ticket.java
@@ -1,35 +1,15 @@
 package com.example.oopaer.models;
 
+@Getter
+@Setter
+
 public class Ticket {
     private static int id_gen=1;
     private String name;
     private int price;
     private int id = id_gen++;
 
-    public Ticket(){
-        this.id = id_gen++;
-    }
 
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String ToString() {
         return id + ". " + "Name of ticket: " + name + " price of ticket: " + price;
